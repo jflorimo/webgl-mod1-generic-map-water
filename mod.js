0cb407d8d98a0f023c496c3ef4bb63471b672424
jflorimo <flojeanc@yahoo.fr>
2015-01-28 14:45:19 +0100

    shadders

--- mod.js
+++ mod.js
@@ -69,6 +69,17 @@ scene = initScene();
 camera  = initCamera();
 light = initLight();
 
+BABYLON.Engine.ShadersRepository = "shaders/";
+
+var skybox = BABYLON.Mesh.CreateSphere("skyBox", 10, 250, scene);
+
+var shader = new BABYLON.ShaderMaterial("gradient", scene, "gradient", {});
+shader.setFloat("offset", 0);
+shader.setFloat("exponent", 0.6);
+shader.setColor3("topColor", BABYLON.Color3.FromInts(0,119,255));
+shader.setColor3("bottomColor", BABYLON.Color3.FromInts(240,240, 255));
+shader.backFaceCulling = false;
+skybox.material = shader;
 /***********************************************************/
 // Fog
 //    var box = new BABYLON.Mesh(name, scene);
